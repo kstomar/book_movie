f8fb48d27e28b809389ccab08a41e4a55cc03d83
koshlendra <koshlendratomar31@gmail.com>
2020-04-30 15:34:15 +0530

    design changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+
+User.create(email: "kaush@yopmail.com", password: "123456789",password_confirmation: "123456789")
+
+Hall.create(name: "one", number_of_row: 2, number_of_column: 3)
+Movie.create(name: "Sholay", genre: "Drama", description: "Lorem Ipsum", format: "A Grade")
+MovieHallShow.create(movie_id: 1, hall_id: 1, show_time: Date.today)
